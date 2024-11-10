f758ae77b68966758fdfbb9c92b8bb8308fc3f42
NeliMasalova <nellymosolova@gmail.com>
2024-11-10 17:56:42 +0100

    [exceptionhandling] Rename ExceptionHandler class to avoid conflict with annotation

--- src/main/java/pl/pingwit/dentalmanager/exceptionhandling/CustomExceptionHandler.java
+++ src/main/java/pl/pingwit/dentalmanager/exceptionhandling/CustomExceptionHandler.java
@@ -2,21 +2,20 @@ package pl.pingwit.dentalmanager.exceptionhandling;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class CustomExceptionHandler {
 
-    // удали org.springframework.web.bind.annotation. и добавь импорт
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler(ValidationException.class)
     public ResponseEntity<String> handleValidationException(ValidationException e) {
         return ResponseEntity.status(BAD_REQUEST).body(e.toString());
     }
 
-    // удали org.springframework.web.bind.annotation. и добавь импорт
-    @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+    @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(NOT_FOUND).body(e.toString());
     }
